c02e32681f339727ee32a12f4683d613b42e8420
LaloEspino <lalospn@gmail.com>
2018-05-23 08:10:00 -0500

    Solicitud

--- Solicitud.cpp
+++ Solicitud.cpp
@@ -13,34 +13,29 @@ Solicitud::Solicitud() {
 
 char * Solicitud::doOperation(char *IP, int puerto, int operationId, char *arguments) {
     
-//    struct mensaje msg;
-//
-//    /* Se pasan los atributos a la estructura msg */
-//
-//    msg.messageType = 0;
-//    msg.requestId = 0;
-//    strcpy(&msg.IP, IP);
-//    msg.puerto = puerto;
-//    msg.operationId = operationId;
-//    memcpy(msg.arguments, arguments, sizeof(char));
-//
-//    /* Se envia la estructura  */
-//
-//    PaqueteDatagrama paq(&msg, sizeof(struct mensaje), IP, puerto);
-//    socketlocal.envia(paq);
-//
-//    char res[10] = "Hola Boom";
-//
-//    return "Hola Boom";
-    
-    PaqueteDatagrama paq(arguments, sizeof(char), IP, puerto);
+    struct mensaje msg;
+
+    /* Se pasan los atributos a la estructura msg */
+
+    msg.messageType = 0;
+    msg.requestId = 0;
+    strcpy(&msg.IP, IP);
+    msg.puerto = puerto;
+    msg.operationId = operationId;
+    memcpy(msg.arguments, arguments, sizeof(char));
+
+    /* Se envia la estructura  */
+
+    PaqueteDatagrama paq(&msg, sizeof(struct mensaje), IP, puerto);
     socketlocal.envia(paq);
-    
+
+    /* Se recibe la respuesta  */
+
     PaqueteDatagrama paq1(sizeof(char));
     socketlocal.recibe(paq1);
-    
+
     char * respuesta = (char *)paq1.obtieneDatos();
-    
+
     return respuesta;
     
 }
