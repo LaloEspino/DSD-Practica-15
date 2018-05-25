92f41ddaa41c22d5647829d7e5350ce3c8e2198b
LaloEspino <lalospn@gmail.com>
2018-05-25 09:20:18 -0500

    Servidor al 💯

--- Respuesta.cpp
+++ Respuesta.cpp
@@ -15,33 +15,63 @@ Respuesta::Respuesta(int pl) {
 
 struct mensaje *Respuesta::getRequest(void) {
     
+    /* Para comprobar que recibio un dato */
     int size = sizeof(struct mensaje);
     PaqueteDatagrama pack(size);
     
     socketlocal->recibe(pack);
+    cout << "¡Ya conecto! 🤓" << endl;
+    cout << "Recibio paquete 📦" << endl;
     
     struct mensaje msg;
     
     // Copia los datos recibidos a una estructura
     memcpy(&msg, pack.obtieneDatos(), size);
+
+    // Imprime valores recibidos
+    cout << "********** Values **********" << endl;
+    cout << "msg.TP: " << msg.messageType << endl;
+    cout << "msg.ID: " << msg.requestId << endl;
+    cout << "msg.IP: " << msg.IP << endl;
+    cout << "msg.PT: " << msg.puerto << endl;
+    cout << "msg.OP: " << msg.operationId << endl;
+    cout << "msg.AR: " << msg.arguments << endl;
+    cout << "****************************" << endl;
+
+    cout << "IP client: " << pack.obtieneDireccion() << endl;
     
-    cout << msg.operationId << endl;
-    
-    
-    
-//    const int size = sizeof(struct mensaje);
-//    PaqueteDatagrama pack(size);
-//    socketlocal->recibe(pack);
-//
-//    struct mensaje msg;
-//
-//    memcpy(&msg, pack.obtieneDatos(), sizeof(struct mensaje));
-//
-//    cout<<msg.operationId<<endl;
+    if (msg.operationId == 1) {
+        
+        int longitud = strlen(msg.arguments);
+        char invertido[longitud];
+
+        for (int x = 0, y = longitud - 1; x < longitud; x ++, y --) {
+            invertido[x] = msg.arguments[y];
+        }
+        invertido[longitud] = '\0';
+        
+        cout << "Invertido 🐶: " << invertido << endl;
+        
+        strcpy(msg.arguments, invertido);
+        
+        cout << "ARGS 🐶: " << msg.arguments << endl;
+    }
     
 	return &msg;
 }
 
 
 void Respuesta::sendReply(char *respuesta, char *ipClient, int puertoCliente) {
+    
+    cout << "Respuesta 🙁: " << respuesta << endl;
+    
+    SocketDatagrama *sck = new SocketDatagrama(puertoCliente);
+    
+    int size = strlen(respuesta);
+    cout << "Tamaño 📏: " << size << endl;
+    
+    PaqueteDatagrama paq(respuesta, size, ipClient, puertoCliente);
+    sck->envia(paq);
+    
+    cout << "Enviado ✈️" << endl;
 }
--- server.cpp
+++ server.cpp
@@ -7,56 +7,24 @@
 
 #include <iostream>
 #include "Respuesta.hpp"
-
+#include "mensaje.hpp"
 
 using namespace std;
 
 int main() {
 
     Respuesta res(9090);
+//    struct mensaje msg;
     
     while (true) {
         cout << "Esperando conexión... 🙄" << endl;
-        res.getRequest();
-        cout << "¡Ya conecto! 🤓" << endl;
+        struct mensaje msg = *res.getRequest();
+        
+        cout << "Ya copia argumentos 🤙🏽 :" << msg.arguments << endl;
+        
+        res.sendReply(msg.arguments, msg.IP, msg.puerto);
+
     }
 
     return 0;
 }
-
-
-// SocketDatagrama socket(7200);
-// unsigned int num = 97;
-// int cont = 0;
-// int *primo[3];
-// while (1) {
-//     PaqueteDatagrama paq(sizeof(num));
-//     //PaqueteDatagrama paq((char *)num,sizeof(unsigned int),ip,7200);
-//     socket.recibe(paq);
-//     cont++;
-//     //num = (int *)paq.obtieneDatos();
-//     unsigned int res[2];
-//     res[0] = num;
-//     res[1] = cont;
-//     //cout << "Res: " << res << endl;
-//     //cout << num[0] <<"+" << num[1] << "=" << res << endl;
-//     PaqueteDatagrama paq1((char *) &res,sizeof(unsigned int),paq.obtieneDireccion(),paq.obtienePuerto());
-//     PaqueteDatagrama paq2((char *) &cont,sizeof(int),paq.obtieneDireccion(),paq.obtienePuerto());
-//     socket.envia(paq1);
-//     socket.envia(paq2);
-    
-//     socket.recibe(paq);
-//     primo[cont - 1] = (int *)paq.obtieneDatos();
-//     cout << "Primo" << cont << " = " << *primo[cont - 1] << endl;
-    
-//     if(cont == 3) {
-//         if(*primo[0] == 1 || *primo[1] == 1 || *primo[2] == 1) {
-//             cout << "El numero NO es primo" << endl;
-//         } else {
-//             cout << "El numero SI es primo" << endl;
-//         }
-//         break;
-//     }
-    
-//     }
-//     return 0;
