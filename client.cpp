738e5513ad5321f682866452e7c6dcb791a30cd7
LaloEspino <lalospn@gmail.com>
2018-05-29 08:27:23 -0500

    ALV

--- client.cpp
+++ client.cpp
@@ -1,38 +1,24 @@
 //
 //  client.cpp
-//  
+//
 //
 //  Created by Lalo Espino on 16/05/18.
 //
 
 #include "Solicitud.hpp"
 #include <iostream>
-#include <stdio.h>      /* printf, fgets */
-#include <stdlib.h>     /* atoi */
 
 using namespace std;
 
-int main(int argc, char * argv[]) {
+int main() {
     
-    if (argc < 1) {
-        return 1;
-    }
-    
-<<<<<<< HEAD
     Solicitud sol;
-
-    char IP[16];
-    strcpy(IP, argv[1]);
-    int port = atoi(argv[2]);
-
-    char cadena[] = "Eduardo Espino y Victor Estrada";
-=======
+    
     char IP[] = "10.100.65.232";
-    char cadena[] = "El dani me la chupa";
->>>>>>> parent of 7071269... ya se entrego
+    char cadena[] = "Eduardo Espino y Victor Estrada";
     int invierteCadena = 1;
     
-    char * respuesta = sol.doOperation(IP, port, invierteCadena, cadena);
+    char * respuesta = sol.doOperation(IP, 9090, invierteCadena, cadena);
     
     cout << respuesta << endl;
     
